cb1cf4a7211be7464ef4842c84a9e84f39b3a81f
harristheharris <anthonyharris7777@gmail.com>
2024-01-28 17:11:18 -0500

    i think we are finding our way through. slowly, but surely.

--- assets/javascipt.js
+++ assets/javascipt.js
@@ -3,12 +3,13 @@
 /* const module = await import("./questions.json", let smplName = {}) */
 /* fetch(/assets/questions.JSON); */
 
-import data from './questions.json' assert {type: 'json'}; 
+import data from './questions.json' assert {type: 'json'};
 console.log(data);
-console.log(data.quizQuestions[0].question1);
+console.log(data.quizQuestions[2]);
 
 var timer = document.querySelector("#timer");
 var strtGme = document.querySelector("#strt-bttn");
+var content = document.querySelector(".content");
 var secondsLeft = 10;
 
 
@@ -16,31 +17,58 @@ var secondsLeft = 10;
 function quizTime() {
   //setTime();
 
-function questGrabber (){
-  var grabQuestions = data.quizQuestions;
-  var rndQuestion = getRandom(grabQuestions);
+  var qAnda = questGrabber();
 
-  // console.log(grabQuestions[0]);
+  console.log(qAnda);
+  console.log(qAnda[0].questions);
+  console.log(qAnda[0].questions.answers);
 
-  // console.log(rndQuestion);
-  console.log(getRandom(grabQuestions));
-  return rndQuestion;
-}
+  var displayQanda = showQuestions(qAnda);
 
-questGrabber();
+  function showQuestions(qAnda) {
+    console.log("We are in the showQuestions functions V V V V ")
+    console.log(qAnda);
+
+  };
 
 
-  
-  
+
 
 
 
 };
 
-function getRandom (list1){
-  var x = Math.floor((Math.random()*list1.length))
-  var ranD = list1[x];
-  var list1 = list1.splice[x , 10];
+
+
+
+
+
+
+
+function questGrabber() {
+  var grabQuestions = data.quizQuestions;
+
+
+  const questIndex = grabQuestions.indexOf(getRandom(grabQuestions));
+  console.log(questIndex);
+  const plchldr = grabQuestions.splice(questIndex, 1);
+
+
+
+  // var rndQuestion = getRandom(grabQuestions);
+  // console.log(grabQuestions[0]);
+  // console.log(rndQuestion);
+  // console.log(getRandom(grabQuestions));
+
+  return plchldr;
+}
+
+
+function getRandom(list1) {
+  // var x = Math.floor((Math.random()*list1.length))
+  var ranD = list1[Math.floor((Math.random() * list1.length))];
+  /* var list1 = list1.splice(x ,1);
+  console.log(list1); */
   // console.log(ranD);
   // console.log(" this is the getRandom function")
   return ranD;
@@ -51,7 +79,7 @@ function getRandom (list1){
 function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function () {
-    
+
 
     secondsLeft--;
     timer.textContent = secondsLeft;
