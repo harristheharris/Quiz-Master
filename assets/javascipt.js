e9e0d299e114d1d8a099f9deca4c5d532fe041a0
harristheharris <anthonyharris7777@gmail.com>
2024-02-17 16:13:34 -0500

    The finishing touch lol: displaying what is in our storage

--- assets/javascipt.js
+++ assets/javascipt.js
@@ -1,39 +1,29 @@
-//Global variables
 
-/* const module = await import("./questions.json", let smplName = {}) */
-/* fetch(/assets/questions.JSON); */
 
 import data from './questions.json' assert {type: 'json'};
 
 console.log(data);
 console.log(data.quizQuestions[2]);
 
+//Global variables
 let score = 0;
 var timer = document.getElementById("timer");
 var strtGme = document.getElementById("strt-bttn");
-// var content = document.querySelector(".content");
 var secondsLeft = 60;
-var questionsLeft = true
+var questionsLeft = true;
+
 //the function that runs once the start game button is pressed
 function quizTime() {
-  setTime();
+
   score = 0;
-
   console.log(score);
-  const questionElement = document.getElementById("question");
-  const answerButton = document.getElementById("answer");
-  // var content = document.getElementById("content");
-
-  document.getElementById("strt-bttn").innerHTML = "";//this is going to throw an error becuase when the program loops and tries to run this line again, there is no strt-bttn to speak of. We already got rid of it. Until I see othewise, I am going to just leave this as it does not have any effect on our code (I think lol)
-  //update: this was originallin in showQuestion. However, because it was throwing an error, our try catch block was always going directly to catch lol. We do not need this to loop so we took it out and have put it here before we start looping
+  document.getElementById("strt-bttn").innerHTML = "";
+  setTime();
   master();
 
 }
 
 function master() {
-
-  // console.log(score);
-
 
   //I wanna try a try/catch block as i know the quiz will error once we run out of questions from our JSON file
   //our catch block will bring us to a GAME OVER function that will log our score and intials within the client side storage
@@ -44,9 +34,7 @@ function master() {
     var qAnda = questGrabber();
 
     //and pick it apart :>
-    // console.log(qAnda);
-    // console.log(qAnda[0].questions);
-    // console.log(qAnda[0].questions.answers);
+   
 
     var displayQanda = showQuestions(qAnda);
     console.log(displayQanda);
@@ -54,33 +42,40 @@ function master() {
   }
    catch {
     questionsLeft = false;
-    // console.log("poop version 2"); 
-    // gameOver();   
+   
   } 
 
 };
-
+//we load the scores from local storage
 function loadScores(){
-  var savedScores = JSON.parse(localStorage.getItem("saved-scores"))
-  if(savedScores === null || savedScores.length ==0){
+  var savedScores = JSON.parse(localStorage.getItem("saved-scores"));//intialize savedScores to equal a JSON parse of getItem("saved-scores") which grabes the value assicaited with (saved-scores) grabbing 
+  console.log(savedScores);
+  if(savedScores === null || savedScores.length ==0){ 
+    //if the savedScores is null (which will be the case if it is the first saved score) OR the savedScores.length equals 0
+    // we leave the function
     return
   }
   //render my stuff
+  console.log(savedScores);
+  return savedScores;
 }
 
 function gameOver (){ 
   console.log("GAME OVER");
   document.getElementById("inside-tup").innerHTML = "";
+  //get the element of our html into a js variable. We are getting our from the id ="big-tup"
   var containerEl = document.getElementById("big-tup");
+
+  //here we are creating div elements our HTML doc
   var yum = document.createElement("div");
   var yummy = document.createElement("div");
-  //containerEl.textContent = "GAME OVER";
   
-  containerEl.insertAdjacentHTML('afterbegin', 'GAME OVER');
-  containerEl.insertAdjacentElement('beforeend', yum);
-  yum.textContent = " you answered " + score + " questions correctly";
-  loadScores()
-  // let userObj = {"score": score, "name": aCh}; //I can do this an easier way that doesnt require JSON Stringify. Won't be as cool tho
+ 
+  containerEl.insertAdjacentHTML('afterbegin', 'GAME OVER'); //we create a container just before the element, before it first child,  
+  containerEl.insertAdjacentElement('beforeend', yum);//and before the end after it's last last child
+  yum.textContent = " you answered " + score + " questions correctly"; // we are setting the the text content property of yum  to the string and displayed score
+  
+ 
   if(confirm("Wouuld you like to save a score")){
     var aCh = prompt("Score: " + score + "   -Insert Name Below-");
     var scoreToSave = {
@@ -90,15 +85,21 @@ function gameOver (){
     saveScore(scoreToSave)
 
   }
-  var nAs = aCh + ": " + score; //I hope javascript just coierces this into a string 
+
+  var loaded = loadScores(); //here we load the scores
+
+  var nAs = aCh + ": " + score; 
   localStorage.setItem("nameAndscore", nAs);
   var subScore = localStorage.getItem("nameAndscore");
-  console.log(subScore); //Jesus christ we finally did it aka we got stuff in our storage object now
+  console.log(subScore); 
   containerEl.insertAdjacentHTML('beforeend', 'HighScores:');
   containerEl.insertAdjacentElement('afterend', yummy);
   yummy.textContent = subScore;
-  yummy.textContent = subScore;
 
+  for (var i = 0; i < loaded.length; i++){
+    console.log(loaded[i]);
+    yummy.innerHTML += `<h3>${loaded[i].name}: ${loaded[i].score}</h3>`
+  }
 
 }
 
@@ -137,18 +138,11 @@ function showQuestions(qAnda) {
 
   });
 
-  /*  console.log(askQuestions); //prints the function
-   console.log(giveAnswers);//prints the answers */
+ 
   document.getElementById("content").innerHTML = askQuestions;
   
 
 
-  // console.log(giveAnswers);
-  /* console.log("We are in the showQuestions functions V V V V ");
-  console.log(qAnda);
-  console.log(askQuestions);
-  console.log(giveAnswers);
- */
 
   function reset() {
 
@@ -157,6 +151,8 @@ function showQuestions(qAnda) {
     }
   };
   
+
+
   function grabbyAnswer(e) {
 
     const slctBtn = e.target;
@@ -183,38 +179,24 @@ function showQuestions(qAnda) {
 
   }
 
-
-
   return giveAnswers;
-
   
 };
 
 function questGrabber() {
   var grabQuestions = data.quizQuestions;
 
-  //WE are grabbing the the index of the object that is chosen and pulling out of the array. the only thing i don't get is how the array isn't updated back to the OG once we leave the functions and comes back
-  //this will come important when we try to figure out how each queston is displayed without repeats
+  //WE are grabbing the the index of the object that is chosen and pulling out of the array. 
 
   const questIndex = grabQuestions.indexOf(getRandom(grabQuestions));
-  // console.log(questIndex);
   const plchldr = grabQuestions.splice(questIndex, 1);
-  // var rndQuestion = getRandom(grabQuestions);
-  // console.log(grabQuestions[0]);
-  // console.log(rndQuestion);
-  // console.log(getRandom(grabQuestions));
-
+ 
   return plchldr;
 }
 
 
 function getRandom(list1) {
-  // var x = Math.floor((Math.random()*list1.length))
   var ranD = list1[Math.floor((Math.random() * list1.length))];
-  /* var list1 = list1.splice(x ,1);
-  console.log(list1); */
-  // console.log(ranD);
-  // console.log(" this is the getRandom function")
   return ranD;
 }
 
@@ -244,9 +226,9 @@ function setTime() {
 
 strtGme.addEventListener("click", quizTime);
 
-var nums = [1,2,3]
-console.log(nums)
-var newNums = nums.push(4)
+// var nums = [1,2,3]
+// console.log(nums)
+// var newNums = nums.push(4)
 
-console.log(newNums)
-console.log(nums)
+// console.log(newNums)
+// console.log(nums)
